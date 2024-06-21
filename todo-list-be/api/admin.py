68d40300e27af2cb22470f666c67f23d71f394a0
Anish Bade <badeanish6045@gmail.com>
2024-06-21 19:31:53 +0545

    created todo model

--- todo-list-be/api/admin.py
+++ todo-list-be/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from api.models import User, Profile
+from api.models import Todo, User, Profile
 
 
 class UserAdmin(admin.ModelAdmin):
@@ -10,6 +10,10 @@ class ProfileAdmin(admin.ModelAdmin):
     list_editable = ["verified"]
     list_display = ["user", "full_name", "verified"]
 
+class TodoAdmin(admin.ModelAdmin):
+    list_editable = ['completed']
+    list_display = ['user', 'title', 'completed', 'date']
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Todo, TodoAdmin)
