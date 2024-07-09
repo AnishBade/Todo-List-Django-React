05bca9732880d0c9d67d52b4cb489ecd641145cf
Anish Bade <badeanish6045@gmail.com>
2024-07-09 22:36:43 +0545

    daily commit

--- todo-list-be/api/views.py
+++ todo-list-be/api/views.py
@@ -75,3 +75,31 @@ class TodoMarkAsCompleted(generics.RetrieveUpdateDestroyAPIView):
         todo.save()
 
         return todo
+
+
+
+class ShippingContainer:
+    next_serial = 1337
+    @classmethod
+    def _get_next_serial(cls):
+        result = cls.next_serial
+        cls.next_serial += 1
+        return result
+    def __init__(self, owner_code, contents):
+        self.owner_code = owner_code
+        self.contents = contents
+        self.serial = ShippingContainer._get_next_serial()
+
+
+
+class ShippingContainer:
+    next_serial = 1337
+    def _get_next_serial():
+        
+        result = ShippingContainer.next_serial
+        ShippingContainer.next_serial += 1
+        return result
+    def __init__(self, owner_code, contents):
+        self.owner_code = owner_code
+        self.contents = contents
+        self.serial = ShippingContainer._get_next_serial()
